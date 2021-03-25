f41de02dbccc6ed9f78c9ac651552133b7c20fe6
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-03-26 00:46:47 +0200

    test

---
+++ Lesson1HM.py
@@ -0,0 +1,30 @@
+# 1:+
+# 2:+
+# 3:
+import this  # подключить модуль подключить модуль this - дзен python
+import antigravity  # подключить модуль "antigravity" -открывает ссылку https://xkcd.com/353/ в браузере по умолчанию
+#import __hello__  # вызвать функцию __hello__ из корневой директории(PyCharm видит как ошибку) - печать Hello world!
+# from __future__ import braces #из функции вызвать модуль(чявооо?!?).так понимаю - шутка программистов- возвращает "SyntaxError: not a chance"
+
+# 4:
+# не по дзен
+
+print('''**** *    **** **** 
+       \r*  * *    *    * 
+       \r*  * *    ***  * 
+       \r*  * *    *    *  * 
+       \r**** **** **** ****''')
+# 4*:
+from art import tprint
+
+tprint("OLEG", "*")
+
+# 5:
+x = int(input("input x:"))
+y = int(input("input y:"))
+
+print(x, y)
+print(x + y)
+print(int(x / y))
+print(x % y)
+print(x ** y)
