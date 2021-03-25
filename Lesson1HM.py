76fe5ef151cd3a3071fdd31c9e5aa2689eb8e18b
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-03-26 01:00:11 +0200

    Update Lesson1HM.py

--- Lesson1HM.py
+++ Lesson1HM.py
@@ -1,7 +1,7 @@
 # 1:+
 # 2:+
 # 3:
-import this  # подключить модуль подключить модуль this - дзен python
+import this  # подключить модуль this - дзен python
 import antigravity  # подключить модуль "antigravity" -открывает ссылку https://xkcd.com/353/ в браузере по умолчанию
 #import __hello__  # вызвать функцию __hello__ из корневой директории(PyCharm видит как ошибку) - печать Hello world!
 # from __future__ import braces #из функции вызвать модуль(чявооо?!?).так понимаю - шутка программистов- возвращает "SyntaxError: not a chance"
