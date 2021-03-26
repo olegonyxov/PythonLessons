782c09359b72f9238499b2ab68c6c4f5f8200283
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-03-26 21:20:58 +0200

    26.03 21:08

--- Lesson_2/HWtask3_1.py
+++ Lesson_2/HWtask3_1.py
@@ -2,16 +2,16 @@ t = int(input("input time:"))
 v = int(input("input Vasya's sspeed:"))
 z = v * t
 if v > 0 and 100 > z > 0:
-    print(z, "'th km")
+    print("Vasia is at", z, "'th km")
 else:
     print("уехал домой")
 while 100 > z > 0:
-    t = int(input("input time:"))
-    v = int(input("input Vasya's sspeed:"))
+    t = int(input("input time to search for Vasia:"))
+    v = int(input("input Vasya's last speed:"))
     z = (v * t) + z
 
     if 100 > z > 0:
-        print(z, "'th km")
+        print("Vasia is at", z, "'th km")
     else:
         print("Васи больше нет :((")
         break
