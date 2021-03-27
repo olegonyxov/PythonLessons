771979ce1ba16e9afedc978bac9c4c3ace2158ed
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-03-27 06:16:49 +0200

    Update HWtask6.py

--- Lesson_2/HWtask6.py
+++ Lesson_2/HWtask6.py
@@ -3,4 +3,4 @@ x_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 if abs(x) in x_list:
     print("the list contains this number")
 else:
-    print("number is not in list")
+    print("the number is not in list")
