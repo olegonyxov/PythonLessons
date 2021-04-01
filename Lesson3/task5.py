2701079766d6a9848d810fcd36a3eefeeea3b490
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-01 23:38:20 +0300

    try2

--- Lesson3/task5.py
+++ Lesson3/task5.py
@@ -1,8 +1,10 @@
 import random
+
 n = 1
-while n <= 3 
-    xd = int(input("input number 1 to 10:"))
+while n <= 3:
+    xd = int(input("input number 1 to 10 (3 tries):"))
     rd = random.randint(1, 10)
+    n += 1
     if xd == rd:
         print("You Won!!!")
     else:
