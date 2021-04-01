0ae04fc08d44071f3d0f7a8cd615b935df519c30
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-01 12:56:15 +0300

    5555

--- Lesson3/task6.py
+++ Lesson3/task6.py
@@ -2,6 +2,8 @@ xs = int(input("input x start"))
 ys = int(input("input y start"))
 x2 = int(input("input x coord 2"))
 y2 = int(input('input y coord 2'))
+success1 = 0
+success2 = 0
 if 0 < xs and ys and x2 and y2 < 8:
 
     hod1 = (-2, 2)
@@ -12,10 +14,18 @@ if 0 < xs and ys and x2 and y2 < 8:
             yend1 = ys + y
             if xend1 == x2 and yend1 == y2:
                 print('Конь Хадить Г')
-              
+                success1 = 1
+
     for x in hod2:
         xend2 = xs + x
         for y in hod1:
             yend2 = ys + y
             if xend2 == x2 and yend2 == y2:
                 print('Конь Хадить Г')
+                success2 = 1
+else:
+    print('кончилась доска')
+if success1 == 1 or success2 == 1:
+    print("ok")
+else:
+    print("не ок")
