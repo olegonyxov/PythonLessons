47b14d2c5bfd034b7deb61012b8ad48a3f67edd0
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-01 23:50:10 +0300

    try2

--- Lesson3/task6.py
+++ Lesson3/task6.py
@@ -1,11 +1,9 @@
-xs = int(input("input x start:"))
-ys = int(input("input y start:"))
-x2 = int(input("input x coord 2:"))
-y2 = int(input("input y coord 2:"))
-success1 = 0
-success2 = 0
+xs = int(input("input x start"))
+ys = int(input("input y start"))
+x2 = int(input("input x coord 2"))
+y2 = int(input('input y coord 2'))
+success = 0
 if 0 < xs and ys and x2 and y2 < 8:
-
     hod1 = (-2, 2)
     hod2 = (-1, 1)
     for x in hod1:
@@ -13,19 +11,16 @@ if 0 < xs and ys and x2 and y2 < 8:
         for y in hod2:
             yend1 = ys + y
             if xend1 == x2 and yend1 == y2:
-                print('Конь Хадить Г')
-                success1 = 1
-
+                success = 1
     for x in hod2:
         xend2 = xs + x
         for y in hod1:
             yend2 = ys + y
             if xend2 == x2 and yend2 == y2:
-                print('Конь Хадить Г')
-                success2 = 1
+                success = 1
 else:
-    print('кончилась доска')
-if success1 == 1 or success2 == 1:
-    print("ok")
+    print('вне поля')
+if success == 1:
+    print("ход возможен")
 else:
-    print("не ок")
+    print("ход невозможен")
