8d0b88c659c21a94b908cb0c7ba552f8bca56c98
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-01 23:18:00 +0300

    try2

--- Lesson3/task2.py
+++ Lesson3/task2.py
@@ -1,17 +1,4 @@
 xl1 = input("ведите число :")
-xl = float(xl1)
-xl = abs(xl) % 1
-print('дробная часть числа:', xl)
-xl = str(xl)
-xl = list(xl)
-print('первая цифра после десятичной точки:', xl[2])
-# почему не правильно работает ?
-# y = xl**2//xl
-# xl = xl -y
-# тоже нет
-# y1 = float(xl1)
-# z1 = abs(y1) - abs(int(y1))
-# print(z1)
-# z1 = str(z1)
-# z1 = list(z1)
-# print(z1[2])
+x = ('0,'+xl1.split('.')[1])
+print('дробная части числа:', x)
+print('первая цифра после запятой:', x[2])
