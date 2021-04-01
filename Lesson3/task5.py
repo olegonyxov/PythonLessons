e0f90fb32e58dd5c603606bfdddb55ee3e614957
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-01 19:22:42 +0300

    5555

--- Lesson3/task5.py
+++ Lesson3/task5.py
@@ -1,8 +1,9 @@
 import random
-
-xd = int(input("input number 1 to 10:"))
-rd = random.randint(1, 10)
-if xd == rd:
-    print("You Won!!!")
-else:
-    print("You Loose!")
+n = 1
+while n <= 3 
+    xd = int(input("input number 1 to 10:"))
+    rd = random.randint(1, 10)
+    if xd == rd:
+        print("You Won!!!")
+    else:
+        print("You Loose!")
