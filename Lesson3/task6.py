0f8dbdc843497ad810fabafbf178a228a3efe465
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-02 03:31:25 +0300

    try2

--- Lesson3/task6.py
+++ Lesson3/task6.py
@@ -1,26 +1,17 @@
-xs = int(input("input x start"))
-ys = int(input("input y start"))
-x2 = int(input("input x coord 2"))
-y2 = int(input('input y coord 2'))
+xs = int(input("input x start:"))
+ys = int(input("input y start:"))
+x2 = int(input("input x coord 2:"))
+y2 = int(input("input y coord 2:"))
 success = 0
-if 0 < xs and ys and x2 and y2 < 8:
-    hod1 = (-2, 2)
-    hod2 = (-1, 1)
-    for x in hod1:
-        xend1 = xs + x
-        for y in hod2:
-            yend1 = ys + y
-            if xend1 == x2 and yend1 == y2:
-                success = 1
-    for x in hod2:
-        xend2 = xs + x
-        for y in hod1:
-            yend2 = ys + y
-            if xend2 == x2 and yend2 == y2:
+if 0 < xs < 8 and 0 < ys < 8 and 0 < x2 < 8 and 0 < y2 < 8:
+    dlist = (-2, -1, 1, 2)
+    for x in dlist:
+        for y in dlist:
+            if abs(x) != abs(y) and x == x2 - xs and y == y2 - ys:
                 success = 1
 else:
-    print('вне поля')
+    print('out of desk')
 if success == 1:
-    print("ход возможен")
+    print('Correct move!')
 else:
-    print("ход невозможен")
+    print('Incorrect move!')
