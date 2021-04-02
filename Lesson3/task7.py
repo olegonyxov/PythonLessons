68d278c460f3456b61c356f536022d6022b7cf00
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-02 04:35:40 +0300

    try2

--- Lesson3/task7.py
+++ Lesson3/task7.py
@@ -1,11 +1,9 @@
-# import time
 x1 = int(input("введите число:"))
 xlist = list(range(x1 + 1))
 n = 1
 y = 1
-# xtime = time.time()
 while n <= x1:
     y = y * n
     n = n + 1
 print('факториал числа', x1, '=', y)
-# print(time.time()-xtime)
+
