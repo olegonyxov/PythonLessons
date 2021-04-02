f7d63068328aff6bb89d910282fb21fa98a8c4cb
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-02 23:05:01 +0300

    try2

--- Lesson3/task6.py
+++ Lesson3/task6.py
@@ -6,7 +6,7 @@ success = 0
 if 0 < xs < 8 and 0 < ys < 8 and 0 < x2 < 8 and 0 < y2 < 8:
     dlist = (-2, -1, 1, 2)
     for x in dlist:
-        for y in dlist:
+        for y in dlist: 
             if abs(x) != abs(y) and x == x2 - xs and y == y2 - ys:
                 success = 1
 else:
