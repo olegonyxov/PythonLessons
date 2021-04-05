6826e6873f4a654ff804d5d80e54de463a60616c
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-05 10:46:21 +0300

    04.04

--- Lesson_4/task2.py
+++ Lesson_4/task2.py
@@ -5,9 +5,9 @@ while xi != 0:
     xi = int(input('input x :'))
     xlist.append(xi)
     n = n + 1
-print('количество введенных чисел', n - 2)
+print('количество введенных чисел:', n - 2)
 sumx = sum((int(x1) for x1 in xlist))
-print('сумма равна', sumx)
+print('сумма равна:', sumx)
 xlist = xlist[0:-1]
 m = 0
 s = 1
@@ -15,8 +15,8 @@ while m < int(len(xlist)):
     s = s * xlist[m]
     m = m + 1
 print('произведение равно:', s)
-print('среднее арифметическое равно', sumx / int(len(xlist)))
-print('наибольшее число :', max(xlist), 'под индексом ', xlist.index(max(xlist)))
+print('среднее арифметическое равно:', sumx / int(len(xlist)))
+print('наибольшее число:', max(xlist), 'под индексом:', xlist.index(max(xlist)))
 
 evenlist = list()
 oddlist = list()
@@ -25,11 +25,11 @@ for i in xlist:
         evenlist.append(i)
     else:
         oddlist.append(i)
-print('количество четных чисел =', len(evenlist))
-print('количество нечетных чисел =', len(oddlist))
+print('количество четных чисел:', len(evenlist))
+print('количество нечетных чисел:', len(oddlist))
 x1list = list(set(xlist))
 x1list.remove(max(x1list))
-print('второе по величине чмсло', max(x1list))
+print('второе по величине чмсло:', max(x1list))
 rlist = list()
 for i in xlist:
     if i == max(xlist):
