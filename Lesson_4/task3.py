8753cb609c44e8d8750bd9e062a33305f76942b0
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-05 06:41:47 +0300

    04.04

--- Lesson_4/task3.py
+++ Lesson_4/task3.py
@@ -1,5 +1,5 @@
-a = int(input('input a'))
-b = int(input('input b'))
+a = int(input('input a:'))
+b = int(input('input b:'))
 alist = list(range(a, (b + 1)))
 blist = list(range(a, (b - 1), -1))
 if a < b:
---
+++ Lesson_4/task4.py
@@ -0,0 +1,8 @@
+n = int(input('input n:'))
+nlist = range(1, n+1, 1)
+nnlist = str()
+if n <=9:
+    for i in nlist:
+        i=str(i)
+        nnlist = nnlist.__add__(i)
+        print(nnlist)
