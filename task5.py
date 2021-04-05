52a0db36c6b5729de13800889040edfa27140e68
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-05 07:10:16 +0300

    04.04

---
+++ task5.py
@@ -0,0 +1,11 @@
+xstr = '0123456789'
+print(type(xstr))
+print(xstr[2])
+print(xstr[-2])
+print(xstr[0:5])
+print(xstr[0:-2])
+print(xstr[2::2])
+print(xstr[1::2])
+print(xstr[::-1])
+print(xstr[-1::-2])
+print(len(xstr))
