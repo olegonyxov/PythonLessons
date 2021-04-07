560a3f2424f2fe7bada832e6de01ecdc22cf2ff9
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-08 00:28:04 +0300

    07.04

--- Lesson_5/task5.py
+++ Lesson_5/task5.py
@@ -1,9 +1,11 @@
 ab = input("input AB:")
 
+
 def out_sq():
     sab = int(ab) * int(ab)
     pab = int(ab) * 4
-    dab = int(ab)* 2**0.5
+    dab = int(ab) * 2 ** 0.5
     return sab, pab, dab
+
 
 print(out_sq())
