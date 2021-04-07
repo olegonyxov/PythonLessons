9b5e5ea7a3b279a163d887ab2b41c086c4606c65
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-07 22:41:53 +0300

    07.04

--- Lesson_5/task3.py
+++ Lesson_5/task3.py
@@ -1,10 +1,13 @@
-side1 = int(input('введите длинну первой стороны:'))
-side2 = int(input('введите второй стороны :'))
-ty2 = int(input('введите тип фигуры:1 = треугольник, 2= квадрат:'))
-def square_all(side1,side2,ty2):
-    if ty2 == 1:
-        return int(side1*side2/2)
-    else:
-        return int(side1*side1)
+side1 = input('введите длинну первой стороны:')
+side2 = input('введите второй стороны :')
+ty1 = input('если тип квадрат: введите 1')
 
-print('Площадь=',square_all(side1,side2,ty2))
+
+def square_all(side1, side2, ty1):
+    if ty1 == '1':
+        return int(side1) * int(side1)
+    else:
+        return int(side1) * int(side2) / 2
+
+
+print('Площадь=', square_all(side1, side2, ty1))
