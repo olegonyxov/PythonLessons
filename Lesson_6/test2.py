3e6b3d08c1a759578ce281e4a9f201b8cc21d643
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-12 11:21:43 +0300

    11.04

---
+++ Lesson_6/test2.py
@@ -0,0 +1,23 @@
+import json
+import datetime
+
+f = open('C:/acdc.json', 'r')
+album = json.load(f)
+album1 = dict(album)
+a = album1.values()
+a = dict(*a)
+d = dict(a.get('tracks'))
+p = (d.get('track'))
+p=str(p)
+p=p.split('ion\':')
+# x=0
+elist=list()
+for e in p:
+    elist.append(e[2:5])
+    # x=x+1
+del elist[0]
+print(elist)
+lsum=sum([int(xs) for xs in elist])
+print(lsum)
+print(str(datetime.timedelta(seconds=lsum)))
+
