417f78e6ffc30451b70b75d4959cf4ece5a7723a
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-12 11:31:18 +0300

    11.04

--- Lesson_6/test.py
+++ Lesson_6/test.py
@@ -1,5 +1,5 @@
-import json
 import datetime
+import json
 
 f = open('C:/acdc.json', 'r')
 album = json.load(f)
@@ -8,14 +8,13 @@ a = album1.values()
 a = dict(*a)
 d = dict(a.get('tracks'))
 p = (d.get('track'))
-p=str(p)
-p=p.split('ion\':')
-elist=list()
+p = str(p)
+p = p.split('ion\':')
+elist = list()
 for e in p:
     elist.append(e[2:5])
 del elist[0]
 print(elist)
-lsum=sum([int(xs) for xs in elist])
+lsum = sum([int(xs) for xs in elist])
 print(lsum)
 print(str(datetime.timedelta(seconds=lsum)))
-
