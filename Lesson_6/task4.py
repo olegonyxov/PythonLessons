181986070c1d08deaaa3022753e6adf3295eb47e
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-14 12:10:18 +0300

    14.04

--- Lesson_6/task4.py
+++ Lesson_6/task4.py
@@ -1,11 +1,10 @@
-import datetime
 import json
-
+import datetime
 from nested_lookup import nested_lookup
 
-f = open('C:/acdc.json', 'r')  # через with_as через раз тупит
-album = json.load(f)
-results = nested_lookup(key='duration', document=album, wild=True)
-sumres = sum([int(xs) for xs in results])
+with open('C:/acdc.json', 'r') as f:
+    album = json.load(f)
+    results = nested_lookup(key='duration', document=album, wild=True)
+    sumres = sum([int(xs) for xs in results])
 print(sumres)
 print(str(datetime.timedelta(seconds=sumres)))
