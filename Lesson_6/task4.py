eb2966f8ce85c93bf89a7fcaddd906cd6143c256
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-14 11:26:12 +0300

    14.04

--- Lesson_6/task4.py
+++ Lesson_6/task4.py
@@ -1,20 +1,11 @@
 import datetime
 import json
 
+from nested_lookup import nested_lookup
+
 f = open('C:/acdc.json', 'r')
 album = json.load(f)
-album1 = dict(album)
-a = album1.values()
-a = dict(*a)
-d = dict(a.get('tracks'))
-p = (d.get('track'))
-p = str(p)
-p = p.split('ion\':')
-elist = list()
-for e in p:
-    elist.append(e[2:5])
-del elist[0]
-print(elist)
-lsum = sum([int(xs) for xs in elist])
-print(lsum)
-print(str(datetime.timedelta(seconds=lsum)))
+results = nested_lookup(key='duration', document=album, wild=True)
+sumres = sum([int(xs) for xs in results])
+print(sumres)
+print(str(datetime.timedelta(seconds=sumres)))
