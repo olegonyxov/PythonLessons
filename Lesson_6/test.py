b9ab132d20f3d03c730b2210a2237e0fca8f04cd
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-18 14:48:26 +0300

    finally

--- Lesson_6/test.py
+++ Lesson_6/test.py
@@ -1,12 +1,1 @@
-import datetime
-import json
 
-result = list()
-with open('C:/acdc.json', 'r') as f1:
-    allalbum = json.load(f1)
-    print(f1.read())
-    for i in allalbum['album']['tracks']['track']:
-        result.append(i['duration'])
-sumres = sum([int(xs) for xs in result])
-print(sumres)
-print(str(datetime.timedelta(seconds=sumres)))
