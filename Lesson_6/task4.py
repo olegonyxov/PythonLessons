3ae58e718f94a735e467efb4f9c4a570855946a6
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-18 14:49:31 +0300

    finally

--- Lesson_6/task4.py
+++ Lesson_6/task4.py
@@ -4,7 +4,6 @@ import json
 result = list()
 with open('C:/acdc.json', 'r') as f1:
     allalbum = json.load(f1)
-    print(f1.read())
     for i in allalbum['album']['tracks']['track']:
         result.append(i['duration'])
 sumres = sum([int(xs) for xs in result])
