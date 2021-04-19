2bb5b85d374dd0357c30ddacddc2dd56d5da20f5
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-19 18:06:10 +0300

    Не успэль

---
+++ Lesson_7/task3.py
@@ -0,0 +1,13 @@
+import requests
+import datetime
+import json
+
+url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Odesa&cnt=5&units=metric&appid=f9ada9efec6a3934dad5f30068fdcbb8"
+response = requests.get(url).json()
+# print(response)
+# print(response.keys())
+# print(response['list'])
+for i in response['list']:
+    print(i['dt'], (i['temp']['day']), (i['feels_like']['day']))
+    # print(str(datetime.timedelta(seconds=i['dt'])))
+
