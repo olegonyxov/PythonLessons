090c1e6b5adbfb7482bf3293e2868770c1ae07ff
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-20 00:40:16 +0300

    20.04

--- Lesson_7/task3.py
+++ Lesson_7/task3.py
@@ -1,17 +1,18 @@
-import requests
 import datetime
-import json
-f2 = open('C:/tr1/t7_3.dat', 'w+')
-f2.writelines('Дата Температура днем  По ощущениям')
+
+import requests
+
+vline = str()
+f2 = open('C:/tr1/t7_3.txt', 'w+')
+f2.writelines('Дата    Температура днем  По ощущениям')
 url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Odesa&cnt=5&units=metric&appid=f9ada9efec6a3934dad5f30068fdcbb8"
 response = requests.get(url).json()
 for i in response['list']:
     day_z = str(datetime.datetime.fromtimestamp(i['dt']))
-    z = day_z[0:10], (i['temp']['day']), (i['feels_like']['day'])
-    print(z)
+    zline = day_z[0:10], "   ", (i['temp']['day']), "   ", (i['feels_like']['day'])
+    zline = str(zline).replace(",", "").replace('\'', "").replace(')', "").replace('(', "")
     f2.writelines("\n")
-    f2.writelines(str(z))
-
+    f2.writelines(zline)
+f2 = open('C:/tr1/t7_3.txt', 'r')
 print(f2.read())
 f2.close()
-
