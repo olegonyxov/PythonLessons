3ab0181ab5db56cf87606ad1e07a2d94dd53f3b3
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-26 22:07:21 +0300

    26.04

--- Lesson_8/task1.py
+++ Lesson_8/task1.py
@@ -1,18 +1,19 @@
 import argparse
 from datetime import timedelta, datetime
+
 import requests
 
 parser = argparse.ArgumentParser(description="currency_checker")
 parser.add_argument('inputincur', default='USD')
 parser.add_argument('outcur', default='UAH')
 parser.add_argument('amcur', default=100)
-parser.add_argument('start_date')
+parser.add_argument('--start_date', default=str(datetime.now().date()))
 args = parser.parse_args()
 argsdict = vars(args)
 incur = argsdict['inputincur']
 outcur = argsdict['outcur']
 amcur = argsdict['amcur']
-input_date = argsdict['start_date']
+input_date = argsdict['--start_date']
 start_date = datetime.strptime(input_date, '%Y-%m-%d')
 today_date = datetime.now()
 finlist = list()
@@ -37,13 +38,12 @@ def make_daylist():
 
 
 def getresponse():
-    finlist.append(['date','from','to', 'rate ',   'result'])
+    finlist.append(['date', 'from', 'to', 'rate ', 'result'])
     for date in daylist:
         pars = {'from': incur, 'to': outcur, 'amount': amcur, 'date': date}
         response = requests.get(url, params=pars)
         data = response.json()
         finlist.append([data['date'], incur, outcur, amcur, data['info']['rate'], data['result']])
-
 
 
 if __name__ == "__main__":
