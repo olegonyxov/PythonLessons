64bfa83c09415c4254205a916a9fbc919c834263
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-27 02:16:55 +0300

    27.04 2

--- Lesson_9/task1.py
+++ Lesson_9/task1.py
@@ -8,7 +8,7 @@ x = 0
 finlist = list()
 
 with open('C:\\tr1\\tz_opendata_z01012021_po01042021.csv', "r", encoding='utf-8') as f1:
-    finlist.append('D_REG BRAND MODEL COLOR MAKE_YEAR FUEL NEW_REG_NEW')
+    finlist.append(['D_REG BRAND MODEL COLOR MAKE_YEAR FUEL NEW_REG_NEW'])
     csv_reader = csv.reader(f1, delimiter=';')
     for row in csv_reader:
         if m in row or m == None:
@@ -17,7 +17,7 @@ with open('C:\\tr1\\tz_opendata_z01012021_po01042021.csv', "r", encoding='utf-8'
                     if y in row or y == None:
                         endlist = list()
                         endlist.append([row[4],row[7],row[10],row[9],row[14],row[18]])
-                        finlist.append(endlist)
+                        finlist.append(*endlist)
                         x += 1
 
     print(x)
