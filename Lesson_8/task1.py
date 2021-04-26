e82d8573163875aec89d4e1bbb446268d639e174
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-26 21:51:22 +0300

    26.04

--- Lesson_8/task1.py
+++ Lesson_8/task1.py
@@ -1,13 +1,12 @@
 import argparse
-import time
 from datetime import timedelta, datetime
 
 import requests
 
 parser = argparse.ArgumentParser(description="currency_checker")
-parser.add_argument('inputincur',default='USD')
-parser.add_argument('outcur',default='UAH')
-parser.add_argument('amcur',default=str(datetime.now().date))
+parser.add_argument('inputincur', default='USD')
+parser.add_argument('outcur', default='UAH')
+parser.add_argument('amcur', default=str(datetime.now().date))
 parser.add_argument('start_date')
 args = parser.parse_args()
 argsdict = vars(args)
@@ -17,6 +16,7 @@ amcur = argsdict['amcur']
 input_date = argsdict['start_date']
 start_date = datetime.strptime(input_date, '%Y-%m-%d')
 today_date = datetime.now()
+finlist = list()
 daylist = list()
 url = 'https://api.exchangerate.host/convert'
 
@@ -38,14 +38,15 @@ def make_daylist():
 
 
 def getresponse():
-    print('date:    from: to:    rate :    result:')
+    finlist.append(['date','from','to', 'rate ',   'result'])
     for date in daylist:
         pars = {'from': incur, 'to': outcur, 'amount': amcur, 'date': date}
         response = requests.get(url, params=pars)
         data = response.json()
-        print(data['date'], incur, outcur, amcur, data['info']['rate'], data['result'])
-        time.sleep(1)
+        finlist.append([data['date'], incur, outcur, amcur, data['info']['rate'], data['result']])
+
 
 
 if __name__ == "__main__":
     checkdate(), make_daylist(), getresponse()
+    print(finlist)
