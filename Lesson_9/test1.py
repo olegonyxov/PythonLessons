14e54a36f74461801584609fe2e2b0f685b804ac
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-28 04:20:30 +0300

    28.04 2

--- Lesson_9/test1.py
+++ Lesson_9/test1.py
@@ -1,12 +1,12 @@
 import argparse
 import csv
-from pprint import pprint
 
 parser = argparse.ArgumentParser(description="find_a_car")
 parser.add_argument('--year', default=None, nargs='?')
 parser.add_argument('--brand', default=None, nargs='?')
 parser.add_argument('--color', default=None, nargs='?')
 parser.add_argument('--fuel', default=None, nargs='?')
+parser.add_argument('--reg_num', default=1, nargs='?')
 args = parser.parse_args()
 argsdict = vars(args)
 
@@ -14,45 +14,55 @@ brand = argsdict['brand']
 color = argsdict['color']
 year = argsdict['year']
 fuel = argsdict['fuel']
-x = 0
+reg_num = argsdict['reg_num']
+varlist = [brand, year, color, fuel]
 finlist = list()
 
 
-def makelist():
+def make_list():
     with open('C:\\tr1\\tz_opendata_z01012021_po01042021.csv', "r", encoding='utf-8') as f1:
-        finlist.append(['D_REG BRAND MODEL COLOR MAKE_YEAR FUEL NEW_REG_NEW'])
         csv_reader = csv.reader(f1, delimiter=';')
         for row in csv_reader:
-            global x
-            if brand in row or brand == None:
-                if color in row or color == None:
-                    if fuel in row or fuel == None:
-                        if year in row or year == None:
-                            endlist = list()
-                            if len(row[18]) != "":
-                                endlist.append([row[4], row[7], row[10], row[9], row[14], row[18]])
-                                finlist.append(*endlist)
-                                x += 1
-                            else:
-                                endlist.append([row[4], row[7], row[10], row[9], row[14]])
-                                finlist.append(*endlist)
-                                x += 1
-        print(x)
+            for var in varlist:
+                if var in row:
+                    templist = list()
+                    if reg_num == None and row[18] != "":
+                        templist.append([row[4], row[7], row[8], row[10], row[9], row[14], row[18]])
+                        finlist.append(*templist)
+                    else:
+                        templist.append([row[4], row[7], row[8], row[10], row[9], row[14], row[18]])
+                        finlist.append(*templist)
 
 
-def makefile():
-    with open('C:\\tr1\\newfile.csv', "w", encoding='utf-8') as f2:
+def make_name():
+    filename = 'C:\\tr1\\'
+    templist = list()
+    for var in varlist:
+        if var != None:
+            templist.append(var)
+    filename = filename + '-'.join(templist)
+    return filename
+
+
+def make_file():
+    with open(make_name(), "w", encoding='utf-8') as f2:
         csv_writer = csv.writer(f2)
+        csv_writer.writerow(['D_REG', 'BRAND', 'MODEL', 'COLOR', 'MAKEYEAR', 'FUEL', 'NEW_REG_NEW'])
         csv_writer.writerows(finlist)
 
 
-makelist()
-makefile()
-with open('C:\\tr1\\newfile.csv', "r", encoding='utf-8') as f3:
-    csv_reader2 = csv.reader(f3)
-    print(type(csv_reader2))
-    for i in csv_reader2:
-        print(i)
+def readfile():
+    with open('C:\\tr1\\newfile.csv', "r", encoding='utf-8') as f3:
+        csv_reader2 = csv.DictReader(f3)
+        for i in csv_reader2:
+            print(i)
 
-# print(x)
-pprint(finlist)
+
+if __name__ == '__main__':
+    if brand == None and color == None and year == None and fuel == None:
+        print("please enter parameters")
+    else:
+        make_list()
+        make_name()
+        make_file()
+        readfile()
