f0ea1f9b196be20c397a5eca21fc856d376beccf
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-04-29 19:01:27 +0300

    29.04

--- Lesson_10/task1.py
+++ Lesson_10/task1.py
@@ -1,17 +1,19 @@
-
 import csv
-inputstr = ['ШАЛАШ', 'КАЗАК', 'ДЕД', 'ДОХОД', '13231','hgfhjgfjhgfjhjhgfj','dfjh']
-with open('polid.csv', 'w+',encoding='utf-8') as file1:
-    reader=csv.reader(file1)
 
-    writer=csv.writer(file1)
-    while True:
-        inputstring = str(input("Input some string:"))
-        writer.writerow(inputstr)
-        if inputstring == "":
-            break
+inputstr = ['ШАЛАШ', 'КАЗАК', 'ДЕД', 'ДОХОД', '13231', 'hgfhjgfjhgfjhjhgfj', 'dfjh']
 
+with open('C:\\tr1\\polid.csv', 'w+', encoding='utf-8') as file1:
+    reader = csv.reader(file1)
+    writer = csv.writer(file1)
+    writer.writerow(['indent', 'string','asa'])
+    for row[2] in reader.line_num
 
+    # while True:
+    #     inputstring = str(input("Input some string:"))
+    #     if inputstring == "":
+    #         break
+    #     for i in reader['string']:
+    #         writer.writerow(inputstr)
 
 # inputstr = ['ШАЛАШ', 'КАЗАК', 'ДЕД', 'ДОХОД', '13231','hgfhjgfjhgfjhjhgfj','dfjh']
 # inputint = 3
@@ -19,7 +21,3 @@ with open('polid.csv', 'w+',encoding='utf-8') as file1:
 # for i in inputstr:
 #     if i == i[::-1]:
 #         print(i)
-
-
-
-
