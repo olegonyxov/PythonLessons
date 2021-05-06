78723a4bc01d7d3d9f720f378b1254c224125b60
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-07 02:14:09 +0300

    07.05

--- Lesson_10/test2.py
+++ Lesson_10/test2.py
@@ -1,11 +1,42 @@
 import csv
-from pprint import pprint
-inputstr = ['ШАЛАШ', 'КАЗАК', 'ДЕД', 'ДОХОД', '13231', 'hgfhjgfjhgfjhjhgfj', 'dfjh']
 
-with open('C:\\tr1\\polid.csv', 'r+', encoding='utf-8') as file1:
-    reader = csv.reader(file1,delimiter='|')
-    writer = csv.writer(file1,delimiter='|')
-    # writer.writerow(['indent', 'string','asa'])
-    # writer.writerow(['asdadas','asda','asdas'])
-    print(file1.readlines())
+wordlist = ['asdfasdf', 'sdfasdf', 'fghfdgh', 'sdfgdsg', 'dfgsdg', 'asdfdsa', 'fdsasdf', ""]
+# inputstr = input('input your words:')
+# inputindent = input('input indent number:')
+inputindent = int("3")
+fieldnames = ('indent', 'string', 'palindrome')
 
+
+def makelist():
+    while True:
+        inputstr = input('input your words:')
+        wordlist.append(inputstr)
+        if inputstr == "":
+            break
+    return wordlist
+
+
+def write_words():
+    with open('C:\\tr1\\polid.csv', 'r+', encoding='utf-8') as file1:
+        writer = csv.DictWriter(file1, fieldnames=fieldnames)
+        for word in wordlist:
+            if word == word[::-1]:
+                writer.writerow({'indent': make_indent(), "string": word, 'palindrome': 'Yes'})
+            else:
+                writer.writerow({'indent': make_indent(), "string": word, 'palindrome': 'NO'})
+
+
+def make_indent():
+    with open('C:\\tr1\\polid.csv', 'r+', encoding='utf-8') as file1:
+        reader = csv.DictReader(file1, fieldnames=fieldnames)
+        for row in reader:
+            print(row)
+            if inputindent < 0 and abs(inputindent) >= len(row["indent"]):
+                del row['indent']
+                indent = ""
+            else:
+                indent = str((inputindent + len(row['indent'])) * " ")
+    return indent
+
+
+write_words()
