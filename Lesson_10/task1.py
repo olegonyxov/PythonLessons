f56518e9193c51283074a50b41a32cfaa2f22722
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-06 18:30:17 +0300

    06.05

--- Lesson_10/task1.py
+++ Lesson_10/task1.py
@@ -1,13 +1,13 @@
 import csv
 
 inputstr = str(input('input some strong:'))
-inputnum = input("input space arg:")
+inputnum = int(input("input space arg:"))
 while inputstr != "":
     with open('C:\\tr1\\polid.csv', 'w', encoding='utf-8') as file1:
         reader = csv.DictReader(file1, fieldnames=['indent', 'string', 'palindrome'])
         writer = csv.DictWriter(file1, fieldnames=['indent', 'string', 'palindrome'])
         if int(inputnum) > 0:
-            inputnum = " " * int(inputnum)
+            inputnum = str(" " * inputnum)
         else:  # TODO
             inputnum = ""
         if inputstr == inputstr[::-1]:
