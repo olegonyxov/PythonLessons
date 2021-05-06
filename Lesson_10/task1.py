9aaa9af25bfb5bf9ed34eb2997c4d7d2822138e9
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-07 02:13:43 +0300

    07.05

--- Lesson_10/task1.py
+++ Lesson_10/task1.py
@@ -8,12 +8,13 @@ inputstr = "lal"
 while inputstr != "":
     inputstr = str(input('input some strong:'))
     with open('C:\\tr1\\polid.csv', 'r+', encoding='utf-8') as file1:
-        reader = csv.DictReader(file1, fieldnames=['indent', 'string', 'palindrome'])
-        writer = csv.DictWriter(file1, fieldnames=['indent', 'string', 'palindrome'])
+        reader = csv.DictReader(file1,('indent', 'string', 'palindrome'))
+        writer = csv.DictWriter(file1,('indent', 'string', 'palindrome'))
 
         if inputstr == inputstr[::-1]:
             writer.writerow({'string': inputstr})
             writer.writerow({'palindrome': 'YES'})
+
         else:
             writer.writerow({'string': inputstr})
             writer.writerow({'palindrome': 'YES'})
