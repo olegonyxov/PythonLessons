93462cfd6d48e9632e251a1a7275f9cd5fc8d845
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-07 06:29:08 +0300

    07.05

--- Lesson_10/task1.py
+++ Lesson_10/task1.py
@@ -13,23 +13,25 @@ def makelist():
 
 def write_words():
     with open('C:\\tr1\\polid.csv', 'r+', encoding='utf-8') as file1:
-        writer = csv.DictWriter(file1, fieldnames=fieldnames, delimiter=' ')
+        writer = csv.DictWriter(file1, fieldnames=fieldnames, delimiter='|')
+        indent = make_indent()
+        print(indent)
         for word in wordlist:
             if word == word[::-1]:
-                writer.writerow({'indent': make_indent(), "string": word, 'palindrome': 'Yes'})
+                writer.writerow({'indent': indent, "string": word, 'palindrome': 'Yes'})
             else:
-                writer.writerow({'indent': make_indent(), "string": word, 'palindrome': 'NO'})
+                writer.writerow({'indent': indent, "string": word, 'palindrome': 'NO'})
 
 
 def make_indent():
-    indent = ""
+    indent = str(inputindent*" ")
     with open('C:\\tr1\\polid.csv', 'r+', encoding='utf-8') as file1:
-        reader = csv.DictReader(file1, fieldnames=fieldnames, delimiter=' ')
-        for row in reader:
-            if inputindent < 0 and abs(inputindent) >= len(row["indent"]) or inputindent == 0:
-                pass
+        reader = csv.DictReader(file1, fieldnames=fieldnames, delimiter='|')
+        for row in reader:  # TODO line_num
+            if inputindent < 0 and abs(inputindent) >= len(row["indent"]):
+                indent = ''
             else:
-                indent = (len(row['indent']) + int(inputindent)) * " "
+                indent = (len(row['indent']) + int(inputindent)) * ' '
     return indent
 
 
