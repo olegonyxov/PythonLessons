2e690650a6c76c735e3dd10b0534ab30d925d2a7
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-13 02:35:48 +0300

    111

--- Lesson_10/task1.py
+++ Lesson_10/task1.py
@@ -3,9 +3,9 @@ import csv
 
 def makelist():
     while True:
-        inputstr = input('input your words:')
-        wordlist.append(inputstr)
-        if inputstr == "":
+        inputedstring = input('input your words:')
+        wordlist.append(inputedstring)
+        if inputedstring == "":
             del wordlist[-1]
             break
     move_list()
@@ -14,21 +14,20 @@ def makelist():
 
 def write_file():
     with open('C:\\tr1\\polid.csv', 'w+', encoding='utf-8') as file1:
-        writer = csv.DictWriter(file1, fieldnames=fieldnames, delimiter='|')
+        writer = csv.DictWriter(file1, fieldnames=fieldnames, delimiter=':')
         for word in makelist():
             if word == word[::-1]:
-                writer.writerow({"string": word, 'palindrome': 'Yes'})
+                writer.writerow({"string": word, 'palindrome': 'palindrome'})
             else:
-                writer.writerow({"string": word, 'palindrome': 'NO'})
+                writer.writerow({"string": word, 'palindrome': 'simple word'})
 
 
 def move_list():
-    inputmoveint = input('input indent number:')
-    if inputmoveint == "":
+    inputedmovevar = input('input move number:')
+    if inputedmovevar == "":
         pass
-    elif inputmoveint != 0:
-        intm = int(inputmoveint)
-        templist = wordlist[0:intm]
+    elif inputedmovevar != 0:
+        templist = wordlist[0:int(inputedmovevar)]
         wordlist.extend(templist)
         del wordlist[0:len(templist)]
     return wordlist
