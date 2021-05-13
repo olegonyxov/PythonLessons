a8e5acca4c72f9234ea8b843c88e6862a031897b
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-13 10:37:56 +0300

    11

--- Lesson_11/task2.py
+++ Lesson_11/task2.py
@@ -1,23 +1,20 @@
 import random
+
 string = 'somebody_email@gmail.com'
+
 
 def change_char():
     slices = string.split('@')
     endlist = []
     for word in slices:
-        a=0
-        while a < (len(word)/4):
+        a = 0
+        while a < (len(word) / 4):
             i = random.randint(0, (len(word) - 1))
-            word = word.replace(word[i],'*')
-            a+=1
+            word = word.replace(word[i], '*')
+            a += 1
         endlist.append(word)
-    endadress= "@".join(endlist)
+    endadress = "@".join(endlist)
     return endadress
+
+
 print(change_char())
-
-
-
-
-
-
-
