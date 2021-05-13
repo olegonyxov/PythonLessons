1ac27b42c49c77f970d7a9d29674d2414a10e0ae
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-13 09:01:52 +0300

    11

--- Lesson_11/task4.py
+++ Lesson_11/task4.py
@@ -4,11 +4,13 @@ parser = argparse.ArgumentParser(description="currency_checker")
 parser.add_argument('old')
 parser.add_argument('new')
 parser.add_argument('count')
-parser.add_argument('-string')
+parser.add_argument('--string')
 argsvars = vars(parser.parse_args())
 
 
 def renew_string():
     endstring = argsvars['string'].replace(argsvars['old'], argsvars['new'], argsvars['count'])
     return endstring
-# print(renew_string())
+
+
+print(renew_string())
