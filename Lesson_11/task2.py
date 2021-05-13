470b11fcb21b142a97d056d62503dcda7f62da77
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-13 09:40:53 +0300

    11

--- Lesson_11/task2.py
+++ Lesson_11/task2.py
@@ -1,3 +1,22 @@
+import random
 string = 'somebody_email@gmail.com'
 slices = string.split('@')
-print(slices)
+word1=slices[0]
+word2=slices[1]
+
+def change_char():
+    i = random.randint(0, len(word1))
+    a=0
+    while a <3:
+        word1.replace(word1[i],"*")
+        a+=1
+
+change_char()
+print(word1)
+
+
+
+
+
+
+
