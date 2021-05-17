f4158886df3046aec2e9508a926121f7894f5e2e
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-17 05:43:50 +0300

    111

--- Lesson_12/task1.py
+++ Lesson_12/task1.py
@@ -1,33 +1,39 @@
+import uuid
 from datetime import datetime
 
 
 class Bank:
-    accunt_name = str
-    uuid = 1
-    balance = 0
+    account_name = ""
+    UID = uuid.uuid1()
+    balance = 0.00
+    fee = 0.01
     transactions = []
     pass
 
+    def __repr__(self):
+        print("Банковский счет")
+
     def deposit(self, amount):
-        self.balance += (amount + amount * 0.1)
+        self.balance += (amount + amount * self.fee)
         actions = (amount, "deposit", datetime.now().strftime('%Y-%m-%d'))
         self.transactions.append(actions)
 
     def withdraw(self, amount):
-        self.balance -= (amount + amount * 0.1)
+        self.balance -= (amount + amount * self.fee)
         actions = (amount, "withdraw", datetime.now().strftime('%Y-%m-%d'))
         self.transactions.append(actions)
 
-    def showbalance(self):
+    def show_balance(self):
         return print(self.balance)
 
-    def showtrans(self):
+    def show_transactions(self):
         return print(self.transactions)
 
 
-this_bank = Bank()
-
-this_bank.deposit(23)
-this_bank.withdraw(5)
-this_bank.showbalance()
-this_bank.showtrans()
+# if __name__ == "__main__":
+#     this_bank = Bank()
+#     this_bank.deposit(23)
+#     this_bank.withdraw(5)
+#     this_bank.show_balance()
+#     this_bank.show_transactions()
+#     print(this_bank.UID)
