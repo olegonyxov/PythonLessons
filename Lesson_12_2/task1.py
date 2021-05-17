e933a34a806fa08983f0c6b5dc7bc032f02eb001
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-17 09:37:05 +0300

    111

--- Lesson_12_2/task1.py
+++ Lesson_12_2/task1.py
@@ -1,23 +1,50 @@
 import random
+
+
 class House:
-    peoples = random.randint(1, 100)
+    peoples = random.randint(1, 99)
+
     def peoples_count(self, pcount):
         self.peoples += pcount
+
     def get_peoples_c(self):
         return self.peoples
 
+
 class Street:
     houses = random.randint(5, 20)
+    houselist = list(range(1, houses))
+
     def houses_count(self, hcount):
         self.houses += hcount
+
     def get_houses_c(self):
         return self.houses
 
+
 class City:
     streets = random.randint(1, 5)
+    streetlist = list(range(1, streets))
+
     def streets_count(self, scount):
         self.streets += scount
+
     def get_streets_c(self):
         return self.streets
 
 
+def nachepatat_poshitat():
+    peoplelist = []
+    population = 0
+    for i in range(1, House.peoples):
+        peoplelist.append(random.randrange(1, 99))
+    print("Улица", 'Дом', 'Населенме', )
+    for street in City.streetlist:
+        for house in Street.houselist:
+            for peoples in peoplelist:
+                print(street, "\t", house, "\t", peoples)
+                population += peoples
+    print("Количество населения :", population, "человек")
+
+
+nachepatat_poshitat()
--- Lesson_12_2/test2.py
+++ Lesson_12_2/test2.py
@@ -1,21 +1,47 @@
+import random
+
+
 class House:
-    housenumber = int
-    peoples = int
-    def peoples_count(self,pcount):
+    peoples = random.randint(1, 99)
+
+    def peoples_count(self, pcount):
         self.peoples += pcount
+
     def get_peoples_c(self):
         return self.peoples
 
+
 class Street:
-    streetnumber = int
-    houses = []
-    def houses_count(self,hcount):
+    houses = random.randint(5, 19)
+    houselist = list(range(1, houses))
+
+    def houses_count(self, hcount):
         self.houses += hcount
 
+    def get_houses_c(self):
+        return self.houses
 
 
 class City:
-    City_name = ""
-    streets = []
-    def streets_count(self,scount):
+    streets = random.randint(1, 5)
+    streetlist = list(range(1, streets))
+
+    def streets_count(self, scount):
         self.streets += scount
+
+    def get_streets_c(self):
+        return self.streets
+
+
+def nachepatat():
+    peoplelist = []
+    for i in range(1, House.peoples):
+        peoplelist.append(random.randrange(1, 99))
+    print("Улица", 'Дом', 'Населенме', )
+    for street in City.streetlist:
+        for house in Street.houselist:
+            for peoples in House.peoplelist:
+                print(street, "\t", house, "\t", peoples)
+
+
+nachepatat()
