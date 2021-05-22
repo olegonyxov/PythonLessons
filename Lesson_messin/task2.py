dfac49a04951dd1737a5595f6fcdae0ffecd6964
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-22 08:51:38 +0300

    111

--- Lesson_messin/task2.py
+++ Lesson_messin/task2.py
@@ -1,7 +1,5 @@
 import re
 
 str1 = input(str("input phone number:"))
-# some = re.findall(r'\d*\d+|\d+', str1)
-renew = "".join(re.findall(r'\d*\d+|\d+', str1))
-print(renew)
-print("(+380)", renew[0:3], renew[3:6], "-", renew[6:8], "-", renew[8:10])
+renew = "".join(re.findall(r'\d*\d+|\d+', str1))  # думаю, так намного полезней
+print("{} {} {}-{}-{}".format("(+380)", renew[0:3], renew[3:6], renew[6:8], renew[8:10]))
