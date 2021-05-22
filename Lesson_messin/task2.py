41a85fe99a39a16cab8833bf0d9f84f4d73952d1
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-22 07:06:26 +0300

    111

---
+++ Lesson_messin/task2.py
@@ -0,0 +1,5 @@
+import re
+
+str1 = "063-999-99-99"
+strin = re.search(r'\w', str1)
+print(strin)
---
+++ Lesson_messin/test1.py
@@ -0,0 +1,20 @@
+l1 = (6,7)
+
+for i in l1:
+    if i in (1,2,3,4):
+        print(i)
+else:
+        print("no")
+
+
+
+
+
+
+
+
+
+    # if l1[0]in l2 or l1[1]in l2 :
+    #     print(l1[0])
+    # else:
+    #     print("no")
