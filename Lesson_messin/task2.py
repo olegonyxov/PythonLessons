cbac6563f1cc2af03c2dd80c398ad99ce2fd0c6d
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-22 10:46:16 +0300

    111

--- Lesson_messin/task2.py
+++ Lesson_messin/task2.py
@@ -1,5 +1,7 @@
 import re
 
 str1 = input(str("input phone number:"))
-renew = "".join(re.findall(r'\d*\d+|\d+', str1))  # думаю, так намного полезней
+renew = "".join(re.findall(r'\d*\d+|\d+', str1))
+if renew[0] == "3":
+    renew = renew[2:-1]
 print("{} {} {}-{}-{}".format("(+38)", renew[0:3], renew[3:6], renew[6:8], renew[8:10]))
--- Lesson_messin/test3.py
+++ Lesson_messin/test3.py
@@ -1,5 +1,3 @@
 import re
 
-str1="38065-245-430-0asda- asdda -asdas "
-renew = re.findall()
-print(renew)
+str1 = "+38065-245-430-0asda- asdda -asdas "
