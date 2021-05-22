bf1f2948f12f7e98a5ed52044464a2a06148bcbe
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-22 11:12:25 +0300

    111

--- Lesson_messin/task2.py
+++ Lesson_messin/task2.py
@@ -1,7 +1,23 @@
 import re
 
-str1 = input(str("input phone number:"))
-renew = "".join(re.findall(r'\d*\d+|\d+', str1))
-if renew[0] == "3":
-    renew = renew[2:-1]
-print("{} {} {}-{}-{}".format("(+38)", renew[0:3], renew[3:6], renew[6:8], renew[8:10]))
+
+def check_numb(str1):
+    renew = "".join(re.findall(r'\d*\d+|\d+', str1))
+    if renew[0] == "3":
+        renew = renew[2:-1]
+    if len(renew) == 10:
+        print("{} {} {}-{}-{}".format("(+38)", renew[0:3], renew[3:6], renew[6:8], renew[8:10]))
+    else:
+        print("incorrect input")
+
+
+def check_input(str1):
+    if re.findall(r'\d', str1) == re.findall(r'\w', str1):
+        check_numb(str1)
+    else:
+        print("incorrect input")
+
+
+if __name__ == "__main__":
+    str1 = input(str("Input phone number :"))
+    check_input(str1)
--- Lesson_messin/test3.py
+++ Lesson_messin/test3.py
@@ -1,3 +1,22 @@
 import re
 
-str1 = "+38065-245-430-0asda- asdda -asdas "
+str1 = "+38065-245-430-20"
+
+
+def check_numb(str1):
+    renew = "".join(re.findall(r'\d*\d+|\d+', str1))
+    if renew[0] == "3":
+        renew = renew[2:-1]
+    if len(renew) == 10:
+        print("{} {} {}-{}-{}".format("(+38)", renew[0:3], renew[3:6], renew[6:8], renew[8:10]))
+    else:
+        print("incorrect input")
+
+def check_input(str1):
+    if re.findall(r'\d', str1) == re.findall(r'\w', str1):
+        check_numb(str1)
+    else:
+        print("incorrect input")
+
+
+check_input(str1)
