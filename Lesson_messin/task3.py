42cd108cfd312c252fb275f7bd4c419157bcf128
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-24 06:38:31 +0300

    111

--- Lesson_messin/task3.py
+++ Lesson_messin/task3.py
@@ -1,12 +1,31 @@
 import re
-istring = "dasdasdASDASD12312$#@-+="
-if re.search(r"\w[a-z]", istring)[0]:
-    if re.search(r"\w[A-Z]", istring)[0]:
-        if re.search(r"\d", istring)[0]:
-            if re.search(r"\W", istring)[0]:
-                if len(istring)>8:
-                    print("password acceptable")
-# if __name__ == "__main__":
-#     istring = input(str("Input your password:"))
 
-    # istring = "dasdasdASDASD12312$#@-+="
+a = 0
+istring = "dasdasdASDASD12312$#@-+="
+if re.search(r"\w[a-z]", istring):
+    pass
+else:
+    a = 1
+    print("Хотя бы одна маленькая буква")
+if re.search(r"\w[A-Z]", istring):
+    pass
+else:
+    a = 1
+    print("Хотя бы одна большая буква")
+if re.search(r"\d", istring):
+    pass
+else:
+    a = 1
+    print("Хотя бы одна цифра")
+if re.search(r"\W", istring):
+    pass
+else:
+    a = 1
+    print("Хотя бы однид символ $#@-+= ")
+if len(istring) > 8:
+    pass
+else:
+    a = 1
+    print("Хотя бы 8 символов")
+if a == 0:
+    print("password accepted")
--- Lesson_messin/test3.py
+++ Lesson_messin/test3.py
@@ -1,7 +1,4 @@
 import re
-str1 = "aaaaaaaaaSASA31213"
-str = "dasdasdASDASD12312$#@-+="
-str=re.findall(r"\S+",str)
-str=re.findall(r"\S+\D+",str[0])
-str=re.findall(r"\S+\D+\W+",str[0])
-print(str)
+istring = "dasdasdASDASD12312$#@-+="
+a =re.search(r"\W", istring)
+print(a)
