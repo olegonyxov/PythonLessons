6923e1a5fb3407a29c7cf602e9ff5c09fa15ba91
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-24 07:23:58 +0300

    111

--- Lesson_messin/test3.py
+++ Lesson_messin/test3.py
@@ -1,4 +1,35 @@
+import csv
 import re
-istring = "dasdasdASDASD12312$#@-+="
-a =re.search(r"\W", istring)
-print(a)
+
+
+def checkshtring():
+    if str1 == re.match(r'^\w{2}\d{4}\w{2}', str1)[0]:
+        return "OK"
+
+
+def translate():
+    with open(translatefile, "r", encoding='utf-8') as file2:
+        reader = csv.DictReader(file2)
+        for row in reader:
+            if str1[0:2] in row["eng"]:
+                str1.replace((str1[0:2]), row["ukr"])
+
+
+def checkregion():
+    print(checkshtring())
+    if checkshtring() == "OK":
+        with open(filename, "r", encoding='utf-8') as file1:
+            reader = csv.DictReader(file1)
+            for row in reader:
+                if str1[0:2] in (row["Код 2004"], ["Код 2013"]):
+                    print("Номер зарегестрирован в ", row["Регіон"])
+    else:
+        print("Строка не является автомобильным номером")
+
+
+if __name__ == "__main__":
+    str1 = "АН3231АЕ"
+    filename = 'C:\\tr1\\ua_auto.csv'
+    translatefile = 'C:\\tr1\\translateautonumb.csv'
+    translate()
+    checkregion()
