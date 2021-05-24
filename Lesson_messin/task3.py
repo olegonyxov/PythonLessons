6f50ca67a4fbce99ca458c2503da5a08e7c9d695
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-24 06:04:02 +0300

    111

--- Lesson_messin/task3.py
+++ Lesson_messin/task3.py
@@ -1,12 +1,12 @@
 import re
-
+istring = "dasdasdASDASD12312$#@-+="
 if re.search(r"\w[a-z]", istring)[0]:
     if re.search(r"\w[A-Z]", istring)[0]:
         if re.search(r"\d", istring)[0]:
             if re.search(r"\W", istring)[0]:
-                if len(str)>8:
+                if len(istring)>8:
                     print("password acceptable")
-if __name__ == "__main__":
-    istring = input(str("Input your password:"))
+# if __name__ == "__main__":
+#     istring = input(str("Input your password:"))
 
     # istring = "dasdasdASDASD12312$#@-+="
