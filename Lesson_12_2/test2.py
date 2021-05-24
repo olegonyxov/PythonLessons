41bc83bd9f0a9a1cb7e34b817238123ad9d90f4d
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-24 05:44:01 +0300

    111

--- Lesson_12_2/test2.py
+++ Lesson_12_2/test2.py
@@ -1,67 +1,50 @@
-import csv
-
-# from datetime import datetime
-# selldate = datetime.now().strftime('%Y-%m-%d')
-filename = 'C:\\tr1\\inventory.csv'
+import random
 
 
-class Product:
+class House:
+    peoples = random.randint(1, 99)
 
-    def __init__(self, pname, ptype, pprice):
-        self.pname = pname
-        self.ptype = ptype
-        self.pprice = pprice
+    def peoples_count(self, pcount):
+        self.peoples += pcount
 
-    def __repr__(self):  # хрень
-        repre = self.ptype,self.pname,self.pprice
-        return str(repre)
+    def get_peoples_c(self):
+        return self.peoples
 
 
-class Store:
-    balanse = 0
-    storagelist = []
+class Street:
+    houses = random.randint(5, 19)
+    houselist = list(range(1, houses))
 
-    def getfile(self):
+    def houses_count(self, hcount):
+        self.houses += hcount
 
-        with open(filename, "r", encoding='utf-8') as file1:
-            reader = csv.DictReader(file1,delimiter=",")
-            bycond = 5
-            for item in reader:
-                for a in range(bycond):
-                    self.storagelist.append(Product(item["Наименование"], item["Тип"], item["Цена"]))
-        return self.storagelist
-
-    def get_ptype_list(self, ptype=""):
-        ptypelist = []
-        for p in self.storagelist:
-            if p.ptype == ptype or ptype == "":
-                ptypelist.append([p.pname, p.ptype, p.pprice])
-        return ptypelist
-
-    def get_remains_price(self):
-        remains_price = 0
-        for p in self.storagelist:
-            remains_price += int(p.pprice)
-        return remains_price
-
-    def sell_item(self, pname):
-        for p in self.storagelist:
-            if p.pname == pname:
-                self.storagelist.remove(p)
-                self.balanse += int(p.pprice)
-                break
+    def get_houses_c(self):
+        return self.houses
 
 
-kofeita = Store()
-kofeita.getfile()
-# print(kofeita.balanse)
-# print(kofeita.get_ptype_list("coffee"))
-# print(kofeita.get_remains_price())
-# kofeita.sell_item("Эспрессо")
-# print(kofeita.balanse)
-# print(kofeita.get_remains_price())
-# print(Product)
-# print(kofeita.get_remains_price())
-# print(kofeita.get_ptype_list("coffee"))
-# pempa = Product()
-# print(pempa)
+class City:
+    streets = random.randint(1, 5)
+    streetlist = list(range(1, streets))
+
+    def streets_count(self, scount):
+        self.streets += scount
+
+    def get_streets_c(self):
+        return self.streets
+
+
+def nachepatat():
+    peoplelist = []
+    for i in range(1, House.peoples):
+        peoplelist.append(random.randrange(1, 99))
+    print("Улица", 'Дом', 'Населенме', )
+    for street in City.streetlist:
+        for house in Street.houselist:
+            for peoples in House.peoplelist:
+                print(street, "\t", house, "\t", peoples)
+
+
+nachepatat()
+class People:
+    people = random.randint(1, 99)
+    return random.randint(1, 99)
--- Lesson_12_2/test3.py
+++ Lesson_12_2/test3.py
@@ -1,27 +1,39 @@
+
 import random
-
-
+from pprint import pprint
 class House:
-    def __init__(self,people):
-        self.people = people
-
-    def add_people(self,pcount):
-        self.people +=pcount
-
-class Street():
-    def __init__(self):
-        self.houselist=houselist
-    def add_house(self):
-        self.houselist.append(House(people))
-
+    people = ""
+    def peoples_count(self, pcount):
+        self.people += pcount
+    def get_peoples_c(self):
+        return self.people
+class Street:
+    houses = random.randint(5,20)
+    houselist = list(range(1, houses))
+    def houses_count(self, hcount):
+        self.houses += hcount
+    def get_houses_c(self):
+        return print(self.houses)
 class City:
-    def __init__(self):
-        self.streetlist = streetlist
-    def add_street(self):
-        self.streetlist.append(Street)
+    streets = random.randint(1, 5)
+    streetlist = list(range(1, streets))
+    def streets_count(self, scount):
+        self.streets += scount
+    def get_streets_c(self):
+        return self.streets
+
+def full_em():
+    finlist = []
+    for street in City.streetlist:
+        for house in Street.houselist:
+            people = random.randint(1, 99)
+            finlist.append([street,house,people])
+    return finlist
+def print_it():
+    print("Улица",'Дом','Население')
+    for result in full_em():
+        print(result[0],result[1],result[2])
 
 
-
-
-
+print_it()
 
