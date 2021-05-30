27feea91095d9fa748daf9f7d3df84e78b399d8c
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-05-30 19:03:54 +0300

    111

--- Lesson_messin/task2.py
+++ Lesson_messin/task2.py
@@ -6,7 +6,7 @@ def check_numb(pnumber):
     if re.search(r'^\d{3}', renew)[0] == "380":
         renew = renew[2:-1]
     if len(renew) == 10:
-        print("{} {} {}-{}-{}".format("(+38)", renew[0:3], renew[3:6], renew[6:8], renew[8:10]))
+        return "{} {} {}-{}-{}".format("(+38)", renew[0:3], renew[3:6], renew[6:8], renew[8:10])
     else:
         print("incorrect input")
 
@@ -14,6 +14,7 @@ def check_numb(pnumber):
 def check_input(pnumber):
     if re.findall(r'\d', pnumber) == re.findall(r'\w', pnumber):
         check_numb(pnumber)
+        return True
     else:
         print("incorrect input")
 
