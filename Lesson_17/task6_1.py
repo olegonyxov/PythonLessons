feaf7a8a680ab5ac29677884ffd360e43042e5c3
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-06-17 23:26:22 +0300

    tester1

---
+++ Lesson_17/task6_1.py
@@ -0,0 +1,3 @@
+def end_dict(coin, code):
+    edict = dict(zip(coin, code))
+    return edict
--- Lesson_17/test_zipper.py
+++ Lesson_17/test_zipper.py
@@ -1,9 +1,12 @@
 import unittest
-import task1
+
+import task6_1
+
 
 class Ziptest(unittest.TestCase):
     def test_end_dict(self):
-        self.assertEqual(task1.end_dict(3,4),(3,4))
+        self.assertEqual(task6_1.end_dict((3, 4), (5, 6)), {3: 5, 4: 6})
+
 
 if __name__ == "__main__":
     unittest.main
