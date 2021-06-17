634247348fcbb4a77235f01c6f1d34d146f5b1f3
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-06-18 00:04:02 +0300

    tester1

---
+++ Lesson_17/task11_2.py
@@ -0,0 +1,17 @@
+import random
+
+
+def change_char(string):
+    slices = string.split('@')
+    endlist = []
+    for word in slices:
+        b = int(len(word) / 3)
+        for a in range(b):
+            i = random.randint(0, (len(word) - 1))
+            word = word.replace(word[i], '*')
+        endlist.append(word)
+    endadress = "@".join(endlist)
+    return endadress
+
+
+print(change_char('somebody_email@gmail.com'))
