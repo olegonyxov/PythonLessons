e701817d2d106754c7d20078b82f436ab5bcb36e
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-06-18 00:08:01 +0300

    tester1

--- Lesson_17/test_mail.py
+++ Lesson_17/test_mail.py
@@ -1,9 +1,12 @@
 import unittest
+
 import task11_2
+
 
 class Mailtest(unittest.TestCase):
     def test_change_char(self):
         self.assertNotEquals(task11_2.change_char('somebody_email@gmail.com'), 'somebody_email@gmail.com')
+
 
 if __name__ == "__main__":
     unittest.main
