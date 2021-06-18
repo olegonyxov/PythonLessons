03d3136ef7d2b85502e6388e8b4502ba7e1c2a56
onyx <56241508+olegonyxov@users.noreply.github.com>
2021-06-18 03:52:46 +0300

    tester1

--- Lesson_17/test_temp.py
+++ Lesson_17/test_temp.py
@@ -3,9 +3,16 @@ import unittest
 import task7_2
 
 
-class Mailtest(unittest.TestCase):
+class temptest(unittest.TestCase):
     def test_if_c(self):
-        self.assertEqual(task7_2.if_c(10,"c"), "Кельвин:" + str + "Цельсий:" + str + "Фарингейт:" + str)
+        self.assertEqual(task7_2.if_c(10,"c"), "Кельвин:" + "283.5" + "Цельсий:" + "10" + "Фарингейт:" + "50")
+
+    def test_if_k(self):
+        self.assertEqual(task7_2.if_c(10, "k"), "Кельвин:" + "10" + "Цельсий:" + "263.5" + "Фарингейт:" + "442")
+
+    def test_if_f(self):
+        self.assertEqual(task7_2.if_f(10, "f"), "Кельвин:" + "261" + "Цельсий:" + "-23" + "Фарингейт:" + 10)
+
 
 if __name__ == "__main__":
     unittest.main
